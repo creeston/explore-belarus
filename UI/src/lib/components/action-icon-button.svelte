c941524b87fe65df554e9a6dbca70368bc7acdaa
Radzivon Chorny <mityy2012@gmail.com>
2024-08-28 23:05:08 +0200

    Fix action buttons layout during transition

--- UI/src/lib/components/action-icon-button.svelte
+++ UI/src/lib/components/action-icon-button.svelte
@@ -7,38 +7,44 @@
     export let icon: any;
     export let active: boolean;
     export let setActive: () => void;
-    export let setInctive: () => void;
+    export let setInactive: () => void;
     export let tooltipText: string;
 </script>
 
-{#if active}
-    <button class="btn btn-square btn-sm btn-success" on:click={setInctive}>
-        <Icon src={icon} size="20" class="btn-neutral" />
-    </button>
+{#if showTooltipOnHover}
+    <div
+        class="tooltip tooltip-left"
+        data-tip={tooltipText}
+        class:tooltip-open={openTooltip}
+    >
+        <button
+            class="btn btn-square btn-sm"
+            class:btn-success={active}
+            class:btn-ghost={!active}
+            on:click={() => (active ? setInactive() : setActive())}
+        >
+            <Icon
+                src={icon}
+                size="20"
+                class={active ? "btn-neutral" : ""}
+                color={style == "ghost" ? "#fff" : ""}
+            />
+        </button>
+    </div>
 {/if}
 
-{#if !active}
-    {#if showTooltipOnHover}
-        <div
-            class="tooltip tooltip-left"
-            data-tip={tooltipText}
-            class:tooltip-open={openTooltip}
-        >
-            <button
-                class="btn btn-square btn-ghost btn-sm"
-                on:click={setActive}
-            >
-                <Icon
-                    src={icon}
-                    size="20"
-                    color={style == "ghost" ? "#fff" : ""}
-                />
-            </button>
-        </div>
-    {/if}
-    {#if !showTooltipOnHover}
-        <button class="btn btn-square btn-ghost btn-sm" on:click={setActive}>
-            <Icon src={icon} size="20" color={style == "ghost" ? "#fff" : ""} />
-        </button>
-    {/if}
+{#if !showTooltipOnHover}
+    <button
+        class="btn btn-square btn-sm"
+        class:btn-success={active}
+        class:btn-ghost={!active}
+        on:click={() => (active ? setInactive() : setActive())}
+    >
+        <Icon
+            src={icon}
+            size="20"
+            class={active ? "btn-neutral" : ""}
+            color={style == "ghost" ? "#fff" : ""}
+        />
+    </button>
 {/if}
